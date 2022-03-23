6ffeb4c816c790963e72514e6d4e68265fac2930
ShubhamChauhan11 <96437297+ShubhamChauhan11@users.noreply.github.com>
2022-03-23 11:28:37 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo, useState } from "react";
+import Login from "./components/Login"
+import Axios from "axios";
+import { BrowserRouter,Routes, Route } from "react-router-dom";
+import Weather from "./components/Weather";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+  const[input,setInput]= useState("London");
+  const[data, setData]= useState("");
+  const[datatwo, setDataTwo]= useState("");
+  console.log(input);
+  const fetchdata= async()=>{
+    const response= await Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=3fbb2b31fd3e77c536be64abc677a4d1`);
+    
+    if(response){
+      setData(response)
+    }
+    
+  }
+  const fetchdata2= async()=>{
+    const response2= await Axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${input}&units=metric&appid=3fbb2b31fd3e77c536be64abc677a4d1`)
+    if(response2){
+      setDataTwo(response2)
+    }
+  }
+  
+  console.log(data);
+  console.log(datatwo);
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />}/>
+      <Route path="/Weather" element={<Weather data={data} setData={setData} setInput={setInput} fetchdata={fetchdata} input={input} fetchdata2={fetchdata2} datatwo={datatwo}/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
 }
-
 export default App;
